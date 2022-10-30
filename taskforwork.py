47417800a871e3d2dbab1542ac533f80a43f817b
YodaKHV <punisher131@gmail.com>
2022-10-30 18:24:15 +0500

    Добавил список для объектов класса (собирается при построчном чтении файла)

--- taskforwork.py
+++ taskforwork.py
@@ -1,5 +1,16 @@
 import os
 
+#создание класса для овощей
+class Vegitable:
+ 
+    def __init__(self, name, price):
+        self.name = name    # название 
+        self.price = price  # цена
+
+
+vegtbls = list() #пустой список, в котором будут храниться объекты овощей
+
+
 filename = ''
 while filename == '':
     filename = input('Введите имя файла: ')
@@ -9,8 +20,15 @@ else:
 
 try:
     f= open(filename , 'r', encoding='utf-8')
+    #veg = 0
     for line in f:
+        #veg += 1
+        
+        line = line.replace('\n', '')
+        line = line.split(' — ')
+        vegtbls.append(Vegitable(line[0],line[1]))
         print(line)
+        
 except FileNotFoundError:
      print('Файл с таким именем не найден, повторите снова.')
 
