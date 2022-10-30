5583a116b701455ae6b787db1a25c059e21fa185
YodaKHV <punisher131@gmail.com>
2022-10-30 13:16:14 +0500

    Добавлен функционал запроса имени файла

---
+++ taskforwork.py
@@ -0,0 +1,20 @@
+from cmath import e
+import os
+
+filename = ''
+while filename == '':
+    filename = input('Введите имя файла: ')
+else:
+    
+    print("Имя введеого файла " + filename)
+
+try:
+    f= open(filename , 'r', encoding='utf-8')
+    for line in f:
+        print(line)
+except FileNotFoundError:
+     print('Файл с таким именем не найден, повторите снова.')
+
+
+
+input()
